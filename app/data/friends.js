a334156984cf2e169ee85a6e217fdfa1e0b9b1ad
mcollums <collumsmichelle@gmail.com>
2019-07-08 18:48:01 -0500

    Created some available pets

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+```json```
+var availablePets = [
+{
+  "name":"Gloria the Tortoise",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+},
+{
+    "name":"Francis the Mule",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        1,
+        1,
+        4,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]
+},
+{
+    "name":"Chuckles the Chinchilla",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        5,
+        5,
+        4,
+        4,
+        1,
+        5,
+        5,
+        5,
+        5
+      ]
+}];
